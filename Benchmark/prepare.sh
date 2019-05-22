452ef36d2493a0e7a4c6aed8ba2239f6375c13db
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2019-05-22 15:04:49 +0200

    Added benchmark setup script

---
+++ Benchmark/prepare.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install requirements
+sudo apt install ffmpeg
+
+# Download source file
+wget http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4
+ffmpeg -i bbb_sunflower_1080p_30fps_normal.mp4 -ss 00:01:20.0 -t 00:00:10.0 source.mp4
+rm bbb_sunflower_1080p_30fps_normal.mp4
+mv source.mp4 video/
+
+for size in 750 500 250 
+do
+  ffmpeg -i video/source.mp4 -filter:v "crop=1080:1080:420:0,scale=$size:$size" "video/cropped_$size.mp4"
+  mkdir video/frames_$size
+  ffmpeg -i video/cropped_$size.mp4 video/frames_$size/frame%04d.jpg -hide_banner 
+done
+
