3334344489a9d66a7482e2ba4f4a15dfc30bc7ef
LAPTOP-C82OUOKU\wenyu <guwenyu2007@126.com>
2019-06-09 14:48:19 +0200

    Pipelined last benchmark code

---
+++ pytorch-AdaIN-master/option.py
@@ -0,0 +1,47 @@
+import argparse
+
+
+class Options():
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        # Basic options
+        self.parser.add_argument('--content', type=str,
+                            help='File path to the content image')
+        self.parser.add_argument('--content_dir', type=str,
+                            help='Directory path to a batch of content images')
+        self.parser.add_argument('--style', type=str,
+                            help='File path to the style image, or multiple style \
+                            images separated by commas if you want to do style \
+                            interpolation or spatial control')
+        self.parser.add_argument('--style_dir', type=str,
+                            help='Directory path to a batch of style images')
+        self.parser.add_argument('--vgg', type=str, default='models/vgg_normalised.pth')
+        self.parser.add_argument('--decoder', type=str, default='models/decoder.pth')
+
+        # Additional options
+        self.parser.add_argument('--content_size', type=int, default=512,
+                            help='New (minimum) size for the content image, \
+                            keeping the original size if set to 0')
+        self.parser.add_argument('--style_size', type=int, default=512,
+                            help='New (minimum) size for the style image, \
+                            keeping the original size if set to 0')
+        self.parser.add_argument('--crop', action='store_true',
+                            help='do center crop to create squared image')
+        self.parser.add_argument('--save_ext', default='.jpg',
+                            help='The extension name of the output image')
+        self.parser.add_argument('--output', type=str, default='outputNe',
+                            help='Directory to save the output image(s)')
+
+        # Advanced options
+        self.parser.add_argument('--preserve_color', action='store_true',
+                            help='If specified, preserve color of the content image')
+        self.parser.add_argument('--alpha', type=float, default=1.0,
+                            help='The weight that controls the degree of \
+                                     stylization. Should be between 0 and 1')
+        self.parser.add_argument(
+            '--style_interpolation_weights', type=str, default='',
+            help='The weight for blending the style of multiple style images')
+
+    def parse(self):
+        return self.parser.parse_args()
--- pytorch-AdaIN-master/test.py
+++ pytorch-AdaIN-master/test.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import torch
 import torch.nn as nn
@@ -13,6 +12,8 @@ import time
 import net
 from function import adaptive_instance_normalization
 from function import coral
+
+from option import Options
 
 
 def test_transform(size, crop):
@@ -43,155 +44,122 @@ def style_transfer(vgg, decoder, content, style, alpha=1.0,
     feat = feat * alpha + content_f * (1 - alpha)
     return decoder(feat)
 
-begin_time = time.time();
 
-parser = argparse.ArgumentParser()
-# Basic options
-parser.add_argument('--content', type=str,
-                    help='File path to the content image')
-parser.add_argument('--content_dir', type=str,
-                    help='Directory path to a batch of content images')
-parser.add_argument('--style', type=str,
-                    help='File path to the style image, or multiple style \
-                    images separated by commas if you want to do style \
-                    interpolation or spatial control')
-parser.add_argument('--style_dir', type=str,
-                    help='Directory path to a batch of style images')
-parser.add_argument('--vgg', type=str, default='models/vgg_normalised.pth')
-parser.add_argument('--decoder', type=str, default='models/decoder.pth')
-
-# Additional options
-parser.add_argument('--content_size', type=int, default=512,
-                    help='New (minimum) size for the content image, \
-                    keeping the original size if set to 0')
-parser.add_argument('--style_size', type=int, default=512,
-                    help='New (minimum) size for the style image, \
-                    keeping the original size if set to 0')
-parser.add_argument('--crop', action='store_true',
-                    help='do center crop to create squared image')
-parser.add_argument('--save_ext', default='.jpg',
-                    help='The extension name of the output image')
-parser.add_argument('--output', type=str, default='outputNe',
-                    help='Directory to save the output image(s)')
-
-# Advanced options
-parser.add_argument('--preserve_color', action='store_true',
-                    help='If specified, preserve color of the content image')
-parser.add_argument('--alpha', type=float, default=1.0,
-                    help='The weight that controls the degree of \
-                             stylization. Should be between 0 and 1')
-parser.add_argument(
-    '--style_interpolation_weights', type=str, default='',
-    help='The weight for blending the style of multiple style images')
-
-args = parser.parse_args()
-
-do_interpolation = False
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Either --content or --contentDir should be given.
-assert (args.content or args.content_dir)
-# Either --style or --styleDir should be given.
-assert (args.style or args.style_dir)
-
-if args.content:
-    content_paths = [args.content]
-else:
-    content_paths = [os.path.join(args.content_dir, f) for f in
-                     os.listdir(args.content_dir)]
-
-if args.style:
-    style_paths = args.style.split(',')
-    if len(style_paths) == 1:
-        style_paths = [args.style]
+def get_style_path(args):
+    do_interpolation, interpolation_weights = None, None
+    if args.style:
+        style_paths = args.style.split(',')
+        if len(style_paths) == 1:
+            style_paths = [args.style]
+        else:
+            do_interpolation = True
+            assert (args.style_interpolation_weights != ''), \
+                'Please specify interpolation weights'
+            weights = [int(i) for i in args.style_interpolation_weights.split(',')]
+            interpolation_weights = [w / sum(weights) for w in weights]
     else:
-        do_interpolation = True
-        assert (args.style_interpolation_weights != ''), \
-            'Please specify interpolation weights'
-        weights = [int(i) for i in args.style_interpolation_weights.split(',')]
-        interpolation_weights = [w / sum(weights) for w in weights]
-else:
-    style_paths = [os.path.join(args.style_dir, f) for f in
-                   os.listdir(args.style_dir)]
+        style_paths = [os.path.join(args.style_dir, f) for f in
+                       os.listdir(args.style_dir)]
 
-if not os.path.exists(args.output):
-    os.mkdir(args.output)
+    return style_paths, do_interpolation, interpolation_weights
 
-decoder = net.decoder
-vgg = net.vgg
 
-decoder.eval()
-vgg.eval()
+def get_content_path(args):
+    if args.content:
+        content_paths = [args.content]
+    else:
+        content_paths = [os.path.join(args.content_dir, f) for f in
+                         os.listdir(args.content_dir)]
+    return content_paths
 
-decoder.load_state_dict(torch.load(args.decoder))
-vgg.load_state_dict(torch.load(args.vgg))
-vgg = nn.Sequential(*list(vgg.children())[:31])
 
-vgg.to(device)
-decoder.to(device)
+def configuration(args):
+    do_interpolation = False
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-content_tf = test_transform(args.content_size, args.crop)
-style_tf = test_transform(args.style_size, args.crop)
+    # Either --content or --contentDir should be given.
+    assert (args.content or args.content_dir)
+    # Either --style or --styleDir should be given.
+    assert (args.style or args.style_dir)
 
-begin_time2 = 0
-end_time2 = 0
+    content_paths = get_content_path(args)
+    style_paths, do_interpolation, interpolation_weights = get_style_path(args)
 
-# For some reason the first style transfer takes up a whole extra second
-# which ruines the data so we do the first run twice :)
-first_time = True
+    if not os.path.exists(args.output):
+        os.mkdir(args.output)
 
-# 5 styles and 300 content images
-benchmarks = np.ndarray([5, 300])
+    return (do_interpolation, device, content_paths, style_paths, interpolation_weights)
 
-for (idxContent, content_path) in enumerate(content_paths):
-    if do_interpolation:  # one content image, N style image
-        style = torch.stack([style_tf(Image.open(p)) for p in style_paths])
-        content = content_tf(Image.open(content_path)) \
-            .unsqueeze(0).expand_as(style)
 
-        #content = content_tf(Image.open(content)) \
-        #    .unsqueeze(0).expand_as(style)
-        style = style.to(device)
-        content = content.to(device)
+def main():
+    begin_time = time.time()
 
-        begin_time2 = time.time()
+    # Figure out configuration
+    args = Options().parse()
+    do_interpolation, device, content_paths, style_paths, interpolation_weights = configuration(args)
 
-        with torch.no_grad():
-            output = style_transfer(vgg, decoder, content, style,
-                                    args.alpha, interpolation_weights)
+    decoder = net.decoder
+    vgg = net.vgg
 
-        end_time2 = time.time()
+    decoder.eval()
+    vgg.eval()
 
-        # Set timing benchmark
-        benchmarks[idxStyle, idxContent] = end_time2 - begin_time2
+    decoder.load_state_dict(torch.load(args.decoder))
+    vgg.load_state_dict(torch.load(args.vgg))
+    vgg = nn.Sequential(*list(vgg.children())[:31])
 
-        output = output.cpu()
-        output_name = '{:s}/{:s}_interpolation{:s}'.format(
-            args.output, splitext(basename(content_path))[0], args.save_ext)
-        save_image(output, output_name)
+    vgg.to(device)
+    decoder.to(device)
 
-    else:  # process one content and one style
-        for (idxStyle, style_path) in enumerate(style_paths):
-            content = content_tf(Image.open(content_path))
-            style = style_tf(Image.open(style_path))
-            if args.preserve_color:
-                style = coral(style, content)
-            style = style.to(device).unsqueeze(0)
-            content = content.to(device).unsqueeze(0)
+    content_tf = test_transform(args.content_size, args.crop)
+    style_tf = test_transform(args.style_size, args.crop)
 
-            #Begin time
+    begin_time2 = 0
+    end_time2 = 0
+
+    # For some reason the first style transfer takes up a whole extra second
+    # which ruines the data so we do the first run twice :)
+    first_time = True
+
+    # 5 styles and 300 content images
+    benchmarks = np.ndarray([5, 300])
+
+    for (idxContent, content_path) in enumerate(content_paths):
+        if do_interpolation:  # one content image, N style image
+            style = torch.stack([style_tf(Image.open(p)) for p in style_paths])
+            content = content_tf(Image.open(content_path)) \
+                .unsqueeze(0).expand_as(style)
+
+            # content = content_tf(Image.open(content)) \
+            #    .unsqueeze(0).expand_as(style)
+            style = style.to(device)
+            content = content.to(device)
+
             begin_time2 = time.time()
 
             with torch.no_grad():
                 output = style_transfer(vgg, decoder, content, style,
-                                        args.alpha)
-            # End time
+                                        args.alpha, interpolation_weights)
+
             end_time2 = time.time()
 
-            # Do the first run twice to fix first run extra second bug
-            # SORRY FOR THE VERY UGLY WAY TO DO THIS :(
-            if first_time & idxContent == 0 & idxStyle == 0:
+            # Set timing benchmark
+            benchmarks[idxStyle, idxContent] = end_time2 - begin_time2
+
+            output = output.cpu()
+            output_name = '{:s}/{:s}_interpolation{:s}'.format(
+                args.output, splitext(basename(content_path))[0], args.save_ext)
+            save_image(output, output_name)
+
+        else:  # process one content and one style
+            for (idxStyle, style_path) in enumerate(style_paths):
+                content = content_tf(Image.open(content_path))
+                style = style_tf(Image.open(style_path))
+                if args.preserve_color:
+                    style = coral(style, content)
+                style = style.to(device).unsqueeze(0)
+                content = content.to(device).unsqueeze(0)
+
                 # Begin time
                 begin_time2 = time.time()
 
@@ -201,59 +169,78 @@ for (idxContent, content_path) in enumerate(content_paths):
                 # End time
                 end_time2 = time.time()
 
-                first_time = False
+                # Do the first run twice to fix first run extra second bug
+                # SORRY FOR THE VERY UGLY WAY TO DO THIS :(
+                if first_time & idxContent == 0 & idxStyle == 0:
+                    # Begin time
+                    begin_time2 = time.time()
 
-            # Set timing benchmark
-            benchmarks[idxStyle, idxContent] = end_time2 - begin_time2
+                    with torch.no_grad():
+                        output = style_transfer(vgg, decoder, content, style,
+                                                args.alpha)
+                    # End time
+                    end_time2 = time.time()
 
-            output = output.cpu()
+                    first_time = False
 
-            output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(
-                args.output, splitext(basename(content_path))[0],
-                splitext(basename(style_path))[0], args.save_ext
-            )
-            save_image(output, output_name)
+                # Set timing benchmark
+                benchmarks[idxStyle, idxContent] = end_time2 - begin_time2
 
-end_time = time.time();
+                output = output.cpu()
 
-print("The total amount of time for test is", end_time - begin_time, "with the setup:", device.type)
+                output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(
+                    args.output, splitext(basename(content_path))[0],
+                    splitext(basename(style_path))[0], args.save_ext
+                )
+                save_image(output, output_name)
 
-means = []
-stds = []
+    end_time = time.time();
+    print("The total amount of time for test is", end_time - begin_time, "with the setup:", device.type)
 
-total = 0
-f = open("benchmark.txt", "a")
-# for each Style
-for idx1, style in enumerate(benchmarks):
-    print("Style", idx1)
-    f.write("Style " + str(idx1) + "\n")
+    if do_interpolation:  # one content image, N style image
+        write_benchmark(benchmarks)
 
-    # For each content image of this style
-    for idx2, item in enumerate(style):
-        print("The time for item", idx2, "took:", item, "seconds")
-        f.writelines("frame " + str(idx2) + " took " + str(item) + " seconds\n")
+def write_benchmark(benchmarks):
+    means = []
+    stds = []
 
-    # Compute avg and std for the style
-    styleSTD = np.std(style)
-    print("The Standard Deviation for style", idx1, "is",styleSTD)
-    stds.append(styleSTD)
+    f = open("benchmark.txt", "a")
+    # for each Style
+    for idx1, style in enumerate(benchmarks):
+        print("Style", idx1)
+        f.write("Style " + str(idx1) + "\n")
 
-    styleAVG = np.mean(style)
-    print("The Average for style", idx1, "is",styleAVG)
-    means.append(styleAVG)
+        # For each content image of this style
+        for idx2, item in enumerate(style):
+            print("The time for item", idx2, "took:", item, "seconds")
+            f.writelines("frame " + str(idx2) + " took " + str(item) + " seconds\n")
+
+        # Compute avg and std for the style
+        styleSTD = np.std(style)
+        print("The Standard Deviation for style", idx1, "is", styleSTD)
+        stds.append(styleSTD)
+
+        styleAVG = np.mean(style)
+        print("The Average for style", idx1, "is", styleAVG)
+        means.append(styleAVG)
+
+    # Write the style avgs and stds
+    for i in range(5):
+        f.write("Style " + str(i) + " avg: " + str(means[i]) + "\n")
+        f.write("Style " + str(i) + " std: " + str(stds[i]) + "\n")
+
+    totalAVG = np.mean(benchmarks)
+    totalSTD = np.std(benchmarks)
+    print("The total average time took:", totalAVG, "seconds TJOM CJATSHJOEK\n")
+    f.write("total_avg:" + str(totalAVG) + "\n")
+
+    print("The total standard deviation:", totalSTD)
+    f.write("std:" + str(totalSTD) + "\n")
+
+    f.close()
+
+    return totalAVG, totalSTD
 
 
-# Write the style avgs and stds
-for i in range(5):
-    f.write("Style " + str(i) + " avg: " + str(means[i]) + "\n")
-    f.write("Style " + str(i) + " std: " + str(stds[i]) + "\n")
-
-totalAVG = np.mean(benchmarks)
-totalSTD = np.std(benchmarks)
-print("The total average time took:", totalAVG, "seconds TJOM CJATSHJOEK\n")
-f.write("total_avg:" + str(totalAVG) + "\n")
-
-print("The total standard deviation:", totalSTD)
-f.write("std:" + str(totalSTD) + "\n")
-
-f.close()
+if __name__ == "__main__":
+    main()
