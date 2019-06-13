8583b9564eb8eb2037f83cb7b22e95baf2679e14
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2019-06-13 15:57:45 +0200

    Fix styletransferdataset

--- smolnet/styletransferdataset.py
+++ smolnet/styletransferdataset.py
@@ -5,7 +5,7 @@ from torchvision import transforms, utils
 import re
 
 class StyleTransferDataset(Dataset):
-    def __init__(self, dir):
+    def __init__(self, dir, transform=None):
         self.dir = Path(dir)
         self.inputdir = self.dir.joinpath('input')
         self.targetdir = self.dir.joinpath('output')
@@ -15,17 +15,22 @@ class StyleTransferDataset(Dataset):
 
         self.outputpostfix = re.search('[0-9]*(.*)', str(self.targetimages[0].name)).group(1)
 
+        self.transform = transform
+
 
     def __len__(self):
         return len(self.inputimages)
 
     def __getitem__(self, idx):
         input_path = str(self.inputimages[idx])
-        target_path = f"{input_path}{re.search('(.*).jpg', input_path).group(1)}"
+        target_path = f"{re.search('(.*).jpg', input_path).group(1)}{self.outputpostfix}".replace('/input/', '/output/')
 
         input_image = io.imread(input_path)
         target_image = io.imread(target_path)
 
         sample = {'input': input_image, 'target': target_image}
+
+        if self.transform:
+            sample = self.transform(sample)
 
         return sample
